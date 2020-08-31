a4ca73c9a35686dbaf4c004f28d7f0549ce58c17
Carlos <carlosc21c21@gmail.com>
2020-09-01 00:12:38 +0200

    build

---
+++ build/precache-manifest.d339b0aab032da3c1656ec7b277633f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86df478c565d2240c9a40ea7be2754f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1ad382b2cb687912560",
+    "url": "./static/css/main.20176c78.chunk.css"
+  },
+  {
+    "revision": "a38fdcb44c590dfd58bf",
+    "url": "./static/js/2.f173773a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f173773a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ad382b2cb687912560",
+    "url": "./static/js/main.e7d5f016.chunk.js"
+  },
+  {
+    "revision": "2b0a4a1a2255c306f3df",
+    "url": "./static/js/runtime-main.fa26db1f.js"
+  }
+]);
